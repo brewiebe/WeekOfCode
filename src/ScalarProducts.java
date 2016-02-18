c6d5741ab2b488a7f24ac2bcee8bbd012e20099c
brewiebe <brewiebe@gmail.com>
2016-02-17 18:51:56 -0800

    day three - scalar products solved

--- src/ScalarProducts.java
+++ src/ScalarProducts.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 /**
  * Created by brewi on 2016-02-17.
  *
- * Write a function generator, gen, to generate the remaining values for a2a2 through a2n+1a2n+1.
+ * Write a function generator, gen, to generate the remaining values for a2 through a2n+1a2n+1.
  * The values returned by gen describe two-dimensional vectors v1…vnv1…vn, where each sequential
  * pair of values describes the respective xx and yy coordinates for some vector vv in the form x1,y1,x2,y2,…,xn,ynx1,y1,x2,y2,…,xn,yn.
  * In other words, v1=(a2,a3),v2=(a4,a5),…,vn=(a2n,a2n+1)v1=(a2,a3),v2=(a4,a5),…,vn=(a2n,a2n+1).
- * Let SS be the set of scalar products of vivi and vj for each 1≤i,j≤n1≤i,j≤n, where i≠ji≠j.
+ * Let S be the set of scalar products of vi and vj for each 1≤i,j≤n1≤i,j≤n, where i≠j.
  * Determine the number of different residues in S and print the resulting value modulo M.
  */
 public class ScalarProducts {
@@ -52,21 +52,70 @@ public class ScalarProducts {
         }
     }
 
+    public static int scalarProduct(int[] a, int[] b){
+        return (a[0] * b[0]) + (a[1] * b[1]);
+    }
+
+    public static List<Integer> scalarList(List<int[]> vectorList){
+        List<Integer> temp = new ArrayList<>();
+        for(int i = 0; i < vectorList.size(); i++){
+            for(int j = i + 1; j < vectorList.size(); j++){
+                temp.add(scalarProduct(vectorList.get(i), vectorList.get(j)));
+            }
+        }
+        return temp;
+    }
+
+    public static void printProducts(List<Integer> prodList){
+        for (int i: prodList)
+        {
+            System.out.println("Product: " + i);
+        }
+    }
+
+    public static List<Integer> residues(List<Integer> prodList, int mod){
+        ArrayList<Integer> temp = new ArrayList<>();
+        for (int i: prodList) {
+            int residue = i % mod;
+            if(temp.indexOf(residue) == -1){
+                temp.add(residue);
+            }
+        }
+        return temp;
+    }
+
+
+    public static void printResidues(List<Integer> resList){
+        for (int i: resList)
+        {
+            System.out.println("Residue: " + i);
+        }
+    }
 
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int C = in.nextInt();
         int M = in.nextInt();
         int n = in.nextInt();
+
         //generate sequence of numbers for vectors as described above
         int[] vectorsArray = gen(C, M, n);
-        printArray(vectorsArray);
+        //printArray(vectorsArray);
 
         //generate arraylist of vectors for calculating scalar product
         List<int[]> vectors = genVectors(vectorsArray);
-        printVectors(vectors);
+        //printVectors(vectors);
 
         //calculate scalar product of vectors in list
+        //return list of scalar products
+        List<Integer> products = scalarList(vectors);
+        //printProducts(products);
+
+        //return array of unique residues
+        List<Integer> residues = residues(products, M);
+        //printResidues(residues);
+
+        System.out.println(residues.size() % M);
 
     }
 }
